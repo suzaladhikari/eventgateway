5ad83afd5216e404e8384d3892efc1b2d2820414
Peter <adhikarisuzal944@gmail.com>
2026-07-30 11:44:05 -0400

    updated sql

--- db/init.sql
+++ db/init.sql
@@ -1,1 +1,7 @@
-/* This is how the data should look like or in simple terms this creates tables 
+-- This is how the data should look like or in simple terms this creates tables 
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,  -- This is the primary key 
+    username VARCHAR(100),  -- Thi is where the user's names gets stored 
+    email VARCHAR(255), -- This is where the email gets updated
+    password VARCHAR(255) -- This is where the password gets updated
+)
