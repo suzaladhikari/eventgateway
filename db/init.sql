78a4300abf3ac525ff07e17c5529464087badc17
Peter <adhikarisuzal944@gmail.com>
2026-07-31 15:25:07 -0400

    sender is responsible for the event_id

--- db/init.sql
+++ db/init.sql
@@ -1,13 +1,14 @@
 -- This is how the data should look like or in simple terms this creates tables 
 
 CREATE TABLE events (
-    event_id UUID PRIMARY KEY DEFAULT gen_random_uuid(), --Creating a unique id 
+    event_id UUID PRIMARY KEY, --Creating a unique id 
     event_type VARCHAR(100) NOT NULL, 
     payload JSONB NOT NULL,  -- The payload column is one column but can contain many pieces of information 
-    status VARCHAR(100) NOT NULL DEFAULT 'pending', --Is the event processed or not 
-    received_at TIMESTAMPZ DEFAULT CURRENT_TIMESTAMP,
-    processed_at TIMESTAMPZ, 
+    status VARCHAR(100) NOT NULL DEFAULT 'pending' CHECK (status IN ('pending', 'processing', 'completed', 'failed')), --Is the event processed or not 
+    received_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+    processed_at TIMESTAMPTZ, 
     retry_count INT DEFAULT 0
 );
+
 
 
