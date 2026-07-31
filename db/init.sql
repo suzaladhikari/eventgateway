69a499fa1ffdcaf8d36eab5647c5cf7a64c659b5
Peter <adhikarisuzal944@gmail.com>
2026-07-31 10:43:01 -0400

    modified the init

--- db/init.sql
+++ db/init.sql
@@ -1,7 +1,9 @@
 -- This is how the data should look like or in simple terms this creates tables 
 
 CREATE TABLE events (
-    id SERIAL PRIMARY KEY, 
-    id UUID PRIMARY KEY DEFAULT gen_random_uuid()
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    username VARCHAR(100)
 );
+
+
 
