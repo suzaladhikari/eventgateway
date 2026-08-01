bc863c60de5a966085c7015a1a327cf97f227fcf
Peter <adhikarisuzal944@gmail.com>
2026-08-01 08:45:18 -0400

    setup received at and processed_at

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 from sqlalchemy.orm import declarative_base
-from sqlalchemy import Column, Integer, String,Enum
+from sqlalchemy import Column, Integer, String,Enum, DateTime
 from sqlalchemy.dialects.postgresql import UUID, JSONB
+from sqlalchemy.sql import func
 import uuid
 Base = declarative_base()
 class EventGateWay(Base): ## Setting up the blueprint 
@@ -8,8 +9,9 @@ class EventGateWay(Base): ## Setting up the blueprint
     event_id = Column(UUID(as_uuid=True),default=uuid.uuid4, primary_key=True) ## This is the unique eventId
     event_type = Column(String, nullable=False) # What type of event is it ?!
     payload = Column(JSONB, nullable=False) ## Accepts the json format 
-    status = Column(Enum('pending', 'processing', 'completed', 'failed', name = 'event_status'), nullable= False, default='pending' )
-    
+    status = Column(Enum('pending', 'processing', 'completed', 'failed', name = 'event_status'), nullable= False, default='pending' ) ## What is the status
+    received_at = Column(DateTime(timezone=True), server_default=func.now())
+    processed_at = Column(DateTime(timezone=True), nullable=True)
 
         
 
