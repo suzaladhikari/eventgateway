caf66244136308bb17ae8be6d40ae45693f74d5f
Peter <adhikarisuzal944@gmail.com>
2026-08-01 14:24:08 -0400

    update

--- app/models.py
+++ app/models.py
@@ -4,7 +4,7 @@ from sqlalchemy.dialects.postgresql import UUID, JSONB
 from sqlalchemy.sql import func
 import uuid
 Base = declarative_base()
-class EventGateWay(Base): ## Setting up the blueprint 
+class Event(Base): ## Setting up the blueprint 
     __tablename__ = 'events' ## The table name that stores the value is called events 
     event_id = Column(UUID(as_uuid=True),default=uuid.uuid4, primary_key=True) ## This is the unique eventId
     event_type = Column(String, nullable=False) # What type of event is it ?!
