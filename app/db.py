8a98a9d78b3f93ac7119ca6afa402b1df05c389b
Peter <adhikarisuzal944@gmail.com>
2026-08-01 12:10:18 -0400

    successfully connected to the local database

--- app/db.py
+++ app/db.py
@@ -1,4 +1,9 @@
 ### This is where we actually connect the python app to postgres 
 from sqlalchemy import create_engine
-DATABASE_URL = create_engine('postgresql://admin:password@localhost:5432/mydatabase')
+import os 
+from dotenv import load_dotenv
+load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL")
+print(DATABASE_URL)
+
 engine = create_engine(DATABASE_URL) ## Connecting to the local database 
