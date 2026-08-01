41ae221950f780936d2c2c67d8472e979a099faf
Peter <adhikarisuzal944@gmail.com>
2026-08-01 08:36:04 -0400

    setup the status

--- app/models.py
+++ app/models.py
@@ -1,5 +1,5 @@
 from sqlalchemy.orm import declarative_base
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String,Enum
 from sqlalchemy.dialects.postgresql import UUID, JSONB
 import uuid
 Base = declarative_base()
@@ -7,7 +7,10 @@ class EventGateWay(Base): ## Setting up the blueprint
     __tablename__ = 'events' ## The table name that stores the value is called events 
     event_id = Column(UUID(as_uuid=True),default=uuid.uuid4, primary_key=True) ## This is the unique eventId
     event_type = Column(String, nullable=False) # What type of event is it ?!
-    payload = Column(JSONB, nullable=False )
+    payload = Column(JSONB, nullable=False) ## Accepts the json format 
+    status = Column(Enum('pending', 'processing', 'completed', 'failed', name = 'event_status'), nullable= False, default='pending' )
+    
+
         
 
 
