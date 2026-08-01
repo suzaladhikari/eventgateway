3e0bb227811321fd2bf8fb8cd434558d1fc535e1
Peter <adhikarisuzal944@gmail.com>
2026-08-01 08:12:33 -0400

    setup event_id

--- app/models.py
+++ app/models.py
@@ -4,8 +4,7 @@ from sqlalchemy import Column, Integer, String
 Base = declarative_base()
 class EventGateWay(Base): ## Setting up the blueprint 
     __tablename__ = 'events' ## The table name that stores the value is called events 
-    def __init__(self,event_id, event_type, payload, status, received_at, processed_at, retry_count):
-        self.event_id = Column(event_id, primary_key=True)
+    event_id = Column(Integer, primary_key=True)
         
 
 
