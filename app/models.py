99a2b42a0d39b04440431e93fad87686dde328b3
Peter <adhikarisuzal944@gmail.com>
2026-08-01 08:16:10 -0400

    setup event_type and payload

--- app/models.py
+++ app/models.py
@@ -1,10 +1,13 @@
 from sqlalchemy.orm import declarative_base
 from sqlalchemy import Column, Integer, String
-
+from sqlalchemy.dialects.postgresql import UUID, JSONB
+import uuid
 Base = declarative_base()
 class EventGateWay(Base): ## Setting up the blueprint 
     __tablename__ = 'events' ## The table name that stores the value is called events 
-    event_id = Column(Integer, primary_key=True)
+    event_id = Column(UUID(as_uuid=True),default=uuid.uuid4, primary_key=True) ## This is the unique eventId
+    event_type = Column(String, nullable=False) # What type of event is it ?!
+    payload = Column(JSONB, nullable=False )
         
 
 
