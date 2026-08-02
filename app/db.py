7cfb5377819a23ebea52b79fc123b23f2b83d4e2
Peter <adhikarisuzal944@gmail.com>
2026-08-02 09:34:31 -0400

    created generator object for the session

--- app/db.py
+++ app/db.py
@@ -1,9 +1,17 @@
 ### This is where we actually connect the python app to postgres 
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 import os 
 from dotenv import load_dotenv
 load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
-print(DATABASE_URL)
 
 engine = create_engine(DATABASE_URL) ## Connecting to the local database 
+SessionLocal = sessionmaker(bind = engine) ## Creating session 
+
+def get_db():
+    db = SessionLocal() ## This can interact with postgres database through the created session
+    try:
+        yield db 
+    finally: 
+        db.close()
