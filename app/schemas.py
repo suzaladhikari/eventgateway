25c8d4004821d7f794ad93a5896213c7a15273c6
Peter <adhikarisuzal944@gmail.com>
2026-08-02 09:57:33 -0400

    created the eventstatus enum

--- app/schemas.py
+++ app/schemas.py
@@ -1,6 +1,16 @@
 from pydantic import BaseModel 
 from sqlalchemy import Column, Integer, String,Enum, DateTime
 from sqlalchemy.dialects.postgresql import UUID, JSONB
+from enum import Enum
+
+### Only for the event_status
+class EventStatus(str, Enum):
+    PENDING = 'pending'
+    PROCESSING = 'processing'
+    COMPLETED = 'completed'
+    FAILED = 'failed'
+    
+
 
 class DatabaseRequest(BaseModel):
     event_id: UUID
