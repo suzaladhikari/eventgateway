c7e7cb9b11fe6c87e046b83ef58ae6f09ba192cc
Peter <adhikarisuzal944@gmail.com>
2026-08-02 11:20:10 -0400

    done with schemas.py

--- app/schemas.py
+++ app/schemas.py
@@ -1,8 +1,6 @@
 from pydantic import BaseModel, Field
-from typing import Optional
 from uuid import UUID
-from enum import Enum
-from datetime import datetime, timezone
+
 class DatabaseRequest(BaseModel):
     event_id: UUID
     event_type: str
