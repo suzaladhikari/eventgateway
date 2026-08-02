e40a90d906e28b4a21e9d7d06e6e3b523e99fb7f
Peter <adhikarisuzal944@gmail.com>
2026-08-02 10:09:28 -0400

    minimalisitc applied

--- app/schemas.py
+++ app/schemas.py
@@ -1,23 +1,9 @@
 from pydantic import BaseModel, Field
 from typing import Optional
-from sqlalchemy import Column, Integer, String,Enum, DateTime
-from sqlalchemy.dialects.postgresql import UUID, JSONB
+from uuid import UUID
 from enum import Enum
 from datetime import datetime, timezone
-
-### Only for the event_status
-class EventStatus(str, Enum):
-    PENDING = 'pending'
-    PROCESSING = 'processing'
-    COMPLETED = 'completed'
-    FAILED = 'failed'
-
-
-
 class DatabaseRequest(BaseModel):
     event_id: UUID
     event_type: str
     payload: dict 
-    status: EventStatus = EventStatus.PENDING
-    received_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
-    process_at: Optional[datetime]
