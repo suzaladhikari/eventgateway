83880d4c30b6477a14845fda0fe370dafa9f9c5a
Peter <adhikarisuzal944@gmail.com>
2026-08-03 01:29:06 -0400

    updated the sqs instead of local dataabase

--- app/routes/events.py
+++ app/routes/events.py
@@ -4,18 +4,16 @@ from app.models import Event
 from app.schemas import DatabaseRequest
 from sqlalchemy.orm import Session
 from app.db import get_db
-
+from app.sqs_client import send_event_to_queue
 router = APIRouter()
 
 @router.post('/events', status_code=202) ## Event received and processing started
-def create_events(request: DatabaseRequest, db: Session = Depends(get_db)):
+def create_events(request: DatabaseRequest):
 ## Db creates a seesion that makes sure that the connection is made to the database once the request is made
     new_event = Event(
         event_id = request.event_id,
         event_type = request.event_type,
         payload = request.payload
     )
-    db.add(new_event)
-    db.commit()
-    db.refresh(new_event)
+    send_event_to_queue(new_event)
     return {"status":"accepted", "event_id":new_event.event_id}
