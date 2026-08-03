1fe43f6b4b8947c1648b65fa7ea6633cd3166faa
Peter <adhikarisuzal944@gmail.com>
2026-08-03 01:27:07 -0400

    finalized the boto3

--- app/sqs_client.py
+++ app/sqs_client.py
@@ -2,4 +2,11 @@ import boto3
 import json 
 import os 
 
-sqs = boto3.client("sqs", region_name = os.getenv('AWS_REGION'))
+sqs = boto3.client("sqs", region_name = os.getenv('AWS_REGION')) ## Setting up the sqs service for client 
+
+def send_event_to_queue(event_data: dict):
+    response = sqs.send_message(
+        QueueUrl = os.getenv('SQS_QUEUE_URL'), ## Using the url for dropping the request 
+        MessageBody = json.dumps(event_data)
+    )
+    return response 
