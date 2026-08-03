7564479c6de7f770fbcf5661cabf1e5fc586b480
Peter <adhikarisuzal944@gmail.com>
2026-08-03 01:41:02 -0400

    updated to the dict format needed

--- app/routes/events.py
+++ app/routes/events.py
@@ -10,10 +10,10 @@ router = APIRouter()
 @router.post('/events', status_code=202) ## Event received and processing started
 def create_events(request: DatabaseRequest):
 ## Db creates a seesion that makes sure that the connection is made to the database once the request is made
-    new_event = Event(
-        event_id = request.event_id,
-        event_type = request.event_type,
-        payload = request.payload
-    )
-    send_event_to_queue(new_event)
-    return {"status":"accepted", "event_id":new_event.event_id}
+    event_data = {
+        "event_id": request.event_id,
+        "event_type": request.event_type,
+        "payload": request.payload
+    }
+    send_event_to_queue(event_data)
+    return {"status":"accepted", "event_id":event_data['event_id']}
