45fc0c9b0227337a69da955183782d34c2543920
Peter <adhikarisuzal944@gmail.com>
2026-08-04 10:00:26 -0400

    added the dlq

--- worker/consumer.py
+++ worker/consumer.py
@@ -2,5 +2,10 @@
 ## Go to the aws console as a IAM user as well: user id : 8716-4028-5396
 import boto3
 import os 
-
+from dotenv import load_dotenv
+import json 
+load_dotenv()
 sqs = boto3.client("sqs", region_name = os.getenv('AWS_REGION'))
+response = sqs.receive_message(QueueUrl = os.getenv("SQS_QUEUE_URL"))
+dlq = sqs.receive_message(QueueUrl = os.getenv("DLQ_URL"))
+
