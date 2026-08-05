e1be62f116d3c5b225cc55595007dc23988c1440
Peter <adhikarisuzal944@gmail.com>
2026-08-05 12:25:14 -0400

    created function and maximum message count to 10

--- worker/consumer.py
+++ worker/consumer.py
@@ -10,34 +10,36 @@ sys.path.append(
 )
 from dotenv import load_dotenv
 import json 
-from app.models import Event
-from app.database import SessionLocal
+from app.models import Event,Base
+from app.database import SessionLocal, engine
 load_dotenv()
-from app.models import Base 
-from app.database import engine
-print("Creating Engine")
 Base.metadata.create_all(bind = engine)
-print("Engine Created!")
 ## Creating a database session 
 db = SessionLocal()
 ## Engine to connect sessions to the database
 
 sqs = boto3.client("sqs", region_name = os.getenv('AWS_REGION'))
-response = sqs.receive_message(QueueUrl = os.getenv("SQS_QUEUE_URL"), MaxNumberOfMessages = 1, WaitTimeSeconds = 10)
 
-for message in response.get('Messages', []):
-    try: 
-        events = json.loads(message['Body'])
-        new_event = Event(event_id = events['event_id'], event_type = events['event_type'], payload = events['payload']) ## This is how the table should be formatted 
-        db.add(new_event)
-        db.commit()
-        sqs.delete_message(QueueUrl = os.getenv("SQS_QUEUE_URL"), ReceiptHandle = message["ReceiptHandle"])
-        print("Deleted from SQS")
-    except Exception as e:
-        db.rollback()
-        print("Error", e)
+def poll_and_process():
+    db = SessionLocal()
+    try:
+        response = sqs.receive_message(QueueUrl = os.getenv("SQS_QUEUE_URL"), MaxNumberOfMessages = 10, WaitTimeSeconds = 30)
+        for message in response.get('Messages', []):
+            try: 
+                events = json.loads(message['Body'])
+                new_event = Event(event_id = events['event_id'], event_type = events['event_type'], payload = events['payload']) ## This is how the table should be formatted 
+                db.add(new_event)
+                db.commit()
+                sqs.delete_message(QueueUrl = os.getenv("SQS_QUEUE_URL"), ReceiptHandle = message["ReceiptHandle"])
+                print("Deleted from SQS")
+            except Exception as e:
+                db.rollback()
+                print("Error", e)
     finally: 
         db.close()
-         
+
+if __name__ == '__main__':
+    while True: 
+        poll_and_process()
         
 
