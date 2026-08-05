816fe71a7928b6faf71661052db1c4e4aa3fd60c
Peter <adhikarisuzal944@gmail.com>
2026-08-05 08:51:29 -0400

    started creating engine

--- worker/consumer.py
+++ worker/consumer.py
@@ -2,13 +2,24 @@
 ## Go to the aws console as a IAM user as well: user id : 8716-4028-5396
 import boto3
 import os 
+import sys
+sys.path.append(
+    os.path.dirname(
+        os.path.dirname(os.path.abspath(__file__))
+    )
+)
 from dotenv import load_dotenv
 import json 
+from app.models import Event
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
 load_dotenv()
 sqs = boto3.client("sqs", region_name = os.getenv('AWS_REGION'))
 response = sqs.receive_message(QueueUrl = os.getenv("SQS_QUEUE_URL"), MaxNumberOfMessages = 1, WaitTimeSeconds = 10)
 for message in response.get('Messages', []):
     try: 
-        event = json.loads(message['Body'])
+        events = json.loads(message['Body'])
+    
         
 
