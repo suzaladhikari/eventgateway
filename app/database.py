ce7aca474257b7cd3841f794672789a57dbe48f0
Peter <adhikarisuzal944@gmail.com>
2026-08-05 08:55:49 -0400

    changed the location of session

---
+++ app/database.py
@@ -0,0 +1,10 @@
+## This is used to create session to connect to the RDS Database !
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import os 
+from dotenv import load_dotenv
+
+load_dotenv()
+
+engine = create_engine(os.getenv("RDS_DATABASE"))
+SessionLocal = sessionmaker(autocommit = False, autoflush = False, bind = engine)
