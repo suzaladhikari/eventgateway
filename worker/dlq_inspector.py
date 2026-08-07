e402617fa22d48c47997f038a93f87390a0958a0
Peter <adhikarisuzal944@gmail.com>
2026-08-07 10:20:34 -0400

    got accesss to all the evnent_Id !

--- worker/dlq_inspector.py
+++ worker/dlq_inspector.py
@@ -5,4 +5,11 @@ load_dotenv()
 sqs = boto3.client("sqs", region_name = os.getenv("AWS_REGION"))
 DLQ_URL = os.getenv("DLQ_URL")
 
-response = sqs.receive_message(QueueUrl = DLQ_URL, MaxNumberOfMessages = 10, WaitTimeSeconds = 5)
+while True: 
+    response = sqs.receive_message(QueueUrl = DLQ_URL, MaxNumberOfMessages = 10, WaitTimeSeconds = 5)
+    messages = response.get('Messages', [])
+    if not messages: 
+        break
+    for message in messages:
+        body = json.loads(message['Body'])
+        print(body.get('event_id'))
