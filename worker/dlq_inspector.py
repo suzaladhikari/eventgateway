d0631693ee892431c25ec1ec616f554cba04e6e5
Peter <adhikarisuzal944@gmail.com>
2026-08-07 10:17:19 -0400

    received the message

--- worker/dlq_inspector.py
+++ worker/dlq_inspector.py
@@ -4,3 +4,5 @@ load_dotenv()
 
 sqs = boto3.client("sqs", region_name = os.getenv("AWS_REGION"))
 DLQ_URL = os.getenv("DLQ_URL")
+
+response = sqs.receive_message(QueueUrl = DLQ_URL, MaxNumberOfMessages = 10, WaitTimeSeconds = 5)
