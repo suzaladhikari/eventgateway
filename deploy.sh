2d4044b257448ffde678b1e4a281630800441244
Peter <adhikarisuzal944@gmail.com>
2026-08-09 17:05:03 -0400

    subtle error fixed

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 set -e 
 REGION=us-east-1 ## Setting up the region 
 ACCOUNT_ID=871640285396 ## Setting up the account_id for IAM user 
-aws ecr get-login-password --region $REGION docker login --username AWS --password-stdin $ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com ## This is how the docker logs in to the aws 
+aws ecr get-login-password --region $REGION | docker login --username AWS --password-stdin $ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com ## This is how the docker logs in to the aws 
 
 ### Producer 
 docker build -f Dockerfile.fastapi -t eventgateway-producer . ## Building the docker image from the dockerfile
