d734f129bbedf470c896211c53de6a3c4b8d78de
Peter <adhikarisuzal944@gmail.com>
2026-08-19 11:10:14 -0400

    specified the dockeriles

--- deploy.sh
+++ deploy.sh
@@ -4,11 +4,11 @@ ACCOUNT_ID=871640285396 ## Setting up the account_id for IAM user
 aws ecr get-login-password --region $REGION | docker login --username AWS --password-stdin $ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com ## This is how the docker logs in to the aws 
 
 ### Producer 
-docker build --platform linux/amd64 -t eventgateway-producer . ## Building the docker image from the dockerfile
+docker build --platform linux/amd64 -f Dockerfile.producer -t eventgateway-producer . ## Building the docker image from the dockerfile
 docker tag eventgateway-producer:latest $ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com/eventgateway-producer:latest ## Naming the image
 docker push $ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com/eventgateway-producer:latest
 
-docker build --platform linux/amd64 -t eventgateway-worker .
+docker build --platform linux/amd64 -f Dockerfile.worker -t eventgateway-worker .
 docker tag eventgateway-worker:latest $ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com/eventgateway-worker:latest
 docker push $ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com/eventgateway-worker:latest 
 
