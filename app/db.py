d466eb1e4db53b8a373980453d2c98be0304c94e
Peter <adhikarisuzal944@gmail.com>
2026-08-19 15:53:27 -0400

    modified the db

--- app/db.py
+++ app/db.py
@@ -5,6 +5,7 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
+print(DATABASE_URL)
 
 engine = create_engine(DATABASE_URL) ## Connecting to the local database 
 SessionLocal = sessionmaker(bind = engine) ## Creating session 
--- app/schemas.py
+++ app/schemas.py
@@ -1,6 +1,5 @@
 from pydantic import BaseModel, Field
 from uuid import UUID
-from app.db import get_db 
 class DatabaseRequest(BaseModel):
     event_id: UUID
     event_type: str
